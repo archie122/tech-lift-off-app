c6783c76f0bbd760126d8b8ef57e72b785e2e93f
Archie <110494298+archie122@users.noreply.github.com>
2024-08-04 23:20:56 -0600

    Updated

--- text_guardian/lib/components/navbar.dart
+++ text_guardian/lib/components/navbar.dart
@@ -28,17 +28,17 @@ class Navbar extends StatelessWidget {
             },
           ),
           ListTile(
+            title: const Text('Results'),
+            onTap: () {
+              Navigator.pushNamed(context, '/results');
+            },
+          ),
+          ListTile(
             title: const Text('Logout'),
             onTap: () {
               Navigator.pushNamed(context, '/login');
             },
           ),
-          ListTile(
-            title: const Text('Results'),
-            onTap: () {
-              Navigator.pushNamed(context, '/results');
-            },
-          )
         ],
       ),
     );
--- text_guardian/lib/components/textbox.dart
+++ text_guardian/lib/components/textbox.dart
@@ -4,22 +4,43 @@ class Textbox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 100,
-      child: TextField(
-        keyboardType: TextInputType.text,
-        decoration: InputDecoration(
-          hintText: 'Enter a product name eg. pension',
-          hintStyle: const TextStyle(fontSize: 14),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8),
-            borderSide: const BorderSide(
-              width: 0,
-              style: BorderStyle.none,
+      height: 150,
+      child: Column(
+        children: [
+          TextField(
+            keyboardType: TextInputType.text,
+            decoration: InputDecoration(
+              hintText: 'Enter the name of the sender',
+              hintStyle: const TextStyle(fontSize: 14),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: const BorderSide(
+                  width: 0,
+                  style: BorderStyle.none,
+                ),
+              ),
+              filled: true,
+              contentPadding: EdgeInsets.all(16),
             ),
           ),
-          filled: true,
-          contentPadding: EdgeInsets.all(16),
-        ),
+          const SizedBox(height: 20),
+          TextField(
+            keyboardType: TextInputType.text,
+            decoration: InputDecoration(
+              hintText: 'Enter the message',
+              hintStyle: const TextStyle(fontSize: 14),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: const BorderSide(
+                  width: 0,
+                  style: BorderStyle.none,
+                ),
+              ),
+              filled: true,
+              contentPadding: EdgeInsets.all(16),
+            ),
+          ),
+        ],
       ),
     );
   }
