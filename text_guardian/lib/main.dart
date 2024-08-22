5c1ef7e410ec39c9d6ed0275541ffe4ba191ae15
Archie <110494298+archie122@users.noreply.github.com>
2024-08-21 23:30:05 -0600

    Got notifications working once again

--- text_guardian/lib/main.dart
+++ text_guardian/lib/main.dart
@@ -1,11 +1,36 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:text_guardian/pages/homepage.dart';
 import 'package:text_guardian/pages/login.dart';
 import 'package:text_guardian/pages/settings.dart';
 import 'package:text_guardian/pages/loading.dart';
-import 'package:text_guardian/pages/results.dart';
+// import 'package:text_guardian/pages/results.dart';
 
-void main() {
+void main() async {
+  await AwesomeNotifications().initialize(null, [
+    NotificationChannel(
+      channelGroupKey: 'basic_channel_group',
+      channelKey: 'basic_channel',
+      channelName: 'Basic notifications',
+      channelDescription: 'Notification channel for basic tests',
+      defaultColor: Colors.teal,
+      importance: NotificationImportance.High,
+      enableLights: true,
+      enableVibration: true,
+      playSound: true,
+    ),
+  ], channelGroups: [
+    NotificationChannelGroup(
+      channelGroupKey: 'basic_channel_group',
+      channelGroupName: 'Basic group',
+    ),
+  ]);
+
+  bool isAllowed = await AwesomeNotifications().isNotificationAllowed();
+  if (!isAllowed) {
+    await AwesomeNotifications().requestPermissionToSendNotifications();
+  }
+
   runApp(const MyApp());
 }
 
